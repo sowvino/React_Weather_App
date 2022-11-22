1f7763e773e09a7bc37662fca801c3b0eb3e54a2
sowvino <106254533+sowvino@users.noreply.github.com>
2022-11-22 10:49:51 +0100

    modified

--- src/container/Api.js
+++ src/container/Api.js
@@ -1,4 +1,4 @@
 
-export const API_KEY = 'KdGMmVqU247UBWyHvoPyGUcMUwxzhM4J';
+export const API_KEY = 'MONFVUjXryQt1FfSUnAsGZtGtbdecG4u';
 export const CITY_URL = `https://dataservice.accuweather.com/locations/v1/cities/search/?`;
 export const WEATHER_URL = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/`;
