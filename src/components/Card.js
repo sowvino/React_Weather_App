de210ed0505204d9a23944d9d8f6e89af0a227e6
sowvino <106254533+sowvino@users.noreply.github.com>
2022-11-22 17:00:43 +0100

    modified

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,13 +13,10 @@ const Card = ({ Max, Min, city, Date, Day, Night, Img, NightImg }) => {
             <h2>{Date}</h2>
             <h3 className="tc bg-moon-gray dib br3 pa3 ma2 grow bw2 shadow-5'"> MAX: {Max}°C  </h3>
             <h3 className="tc bg-moon-gray dib br3 pa3 ma2 grow bw2 shadow-5'"> MIN: {Min}°C</h3>
-            <h3>At Day:   {Day}</h3>
+            <h3>At Day:    {Day}</h3>
             <img src={Img} width='130px' alt="weather image"></img>
-            <h3>At Night:
-                {Night}</h3>
+            <h3>At Night:  {Night}</h3>
             <img src={NightImg} width='130px' alt="weather image"></img>
-
-
         </div>
 
 
